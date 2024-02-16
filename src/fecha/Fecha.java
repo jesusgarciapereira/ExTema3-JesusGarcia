625dd92ed7de7d8aaf92b1b1a7bf9fd61f2b69c0
jesusgarciapereira <jesus.garciap@iesnervion.es>
2024-02-16 10:54:28 +0100

    Primer commit de Javadoc

--- src/fecha/Fecha.java
+++ src/fecha/Fecha.java
@@ -1,24 +1,50 @@
 package fecha;
 
+/**
+ * Clase que representa a una fecha, conteniendo sus atributos, constructores y
+ * funciones
+ */
 public class Fecha {
-	private int d; //día
-	private int m; //mes
-	private int a; //año
+	/**
+	 * Atributo d: Correspondiente al dia
+	 */
+	private int d; // dï¿½a
+	/**
+	 * Atributo m: Correspondiente al mes
+	 */
+	private int m; // mes
+	/**
+	 * Atributo m: Correspondiente al anio
+	 */
+	private int a; // aï¿½o
 
-	
+	/**
+	 * Constructor sin parametros
+	 */
 	public Fecha() {
 
 	}
 
-	
+	/**
+	 * Constructor con todos los atributos como parametros
+	 * 
+	 * @param dia  Correspondiente al dia (atributo d)
+	 * @param mes  Correspondiente al mes (atributo m)
+	 * @param anio Correspondiente al anio (atributo a)
+	 */
 	public Fecha(int dia, int mes, int anio) {
 		this.d = dia;
 		this.m = mes;
 		this.a = anio;
 	}
 
-	
-	
+	/**
+	 * Funcion que indica si una fecha es correcta, atendiendo a que los numeros del
+	 * dia, mes y anio sean los correctos y no sobrepasen los posibles valores que
+	 * puedan tener
+	 * 
+	 * @return True o false segun si la fecha es correcta o no
+	 */
 	public boolean fechaCorrecta() {
 		boolean diaCorrecto, mesCorrecto, anioCorrecto;
 		anioCorrecto = a > 0;
@@ -43,13 +69,21 @@ public class Fecha {
 		return diaCorrecto && mesCorrecto && anioCorrecto;
 	}
 
-	// Método esBisiesto. Solo lo usa fechaCorrecta, por eso es privado
+	/**
+	 * Funcion que indica si un anio es bisiesto, solo lo usa fechaCorrecta, por eso
+	 * es privado
+	 * 
+	 * @return True o false segun si se cumple la condicion con los calculos correspondientes
+	 */
 	private boolean esBisiesto() {
 		boolean esBisiesto = (a % 4 == 0 && a % 100 != 0 || a % 400 == 0);
 		return esBisiesto;
 	}
 
-	// Método diaSiguiente
+	/**
+	 * Funcion que modifica los atributos necesarios para indicar convertir la fecha
+	 * en al dia siguiente
+	 */
 	public void diaSiguiente() {
 		d++;
 		if (!fechaCorrecta()) {
@@ -62,7 +96,10 @@ public class Fecha {
 		}
 	}
 
-	// Método toString
+	/**
+	 * Funcion que devuelve, en formato texto, la fecha correspondiente, anniadiendo
+	 * ceros a la izquierda si es necesario
+	 */
 	public String toString() {
 		if (d < 10 && m < 10) {
 			return "0" + d + "-0" + m + "-" + a;
